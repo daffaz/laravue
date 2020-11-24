664689e64ea9a52598b1bdbd8ac3d34f583a77c2
Daffa Z <67418594+j3c118124@users.noreply.github.com>
2020-11-24 22:52:16 +0700

    init

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function controllerMethod()
+    {
+        return view('welcome');
+    }
+
+    public function testaja()
+    {
+    }
+}
